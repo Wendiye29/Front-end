a721cc8965df4e297df6fcd0a9091d5b999f5a6e
Wendiye29 <girmasossina2@gmail.com>
2025-06-05 14:00:42 -0700

    change on the home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import React from "react";
-export default function Home() {
-    return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Welcome to Habtisho's page</h1>
-        <p>This is the home page.</p>
+"use client";
+import { Carousel } from "antd";
+
+const images = [
+  "/image/b.jpg",
+  "/image/e.jpg",
+  "/image/f.jpg",
+  "/image/j.jpg",   
+  "/image/ty.jpg",
+  "/image/R.jpg",
+  "/image/ty.jpg",
+  "image/u.jpg",
+  
+];
+
+export default function ImageSlider() {
+  return (
+    <Carousel autoplay>
+      {images.map((src, index) => (
+        <div key={index}>
+          <img src={src} alt={`Slide ${index + 1}`} style={{ width: "100%", height: "400px", objectFit: "cover" }} />
         </div>
-    );
-    }
+      ))}
+    </Carousel>
+  );
+}
